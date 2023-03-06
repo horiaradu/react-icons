f7b8262fbb82540c9037aef9ec09fedbaa9ff215
Horia Radu <horia.radu@e-spres-oh.com>
2023-03-06 17:56:12 +0200

    Configurable size

--- components/sick.tsx
+++ components/sick.tsx
@@ -1,8 +1,8 @@
-export function Sick() {
+export function Sick({ width, height }: { width?: number; height?: number }) {
   return (
     <svg
-      width="21"
-      height="20"
+      width={width || 21}
+      height={height || 20}
       viewBox="0 0 21 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
